c38de43d20713df208aeca958f46abdba93a2d61
temt-ceo <taharatakashi@outlook.com>
2022-12-24 14:14:36 +0900

    To configure Nuxt3 + fcl

---
+++ plugins/fcl.client.js
@@ -0,0 +1,15 @@
+import * as fcl from "@onflow/fcl"
+import * as types from '@onflow/types'
+
+fcl.config({
+  "accessNode.api": "https://rest-testnet.onflow.org", // Mainnet: "https://rest.onflow.org"
+  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn", // Mainnet: "https://fcl-discovery.onflow.org/authn",
+  "app.detail.title": "Kitty Items", // Shows user what dapp is trying to connect
+  "app.detail.icon": "https://fcl-discovery.onflow.org/images/blocto.png", // shows image to the user to display your dapp brand
+  "0xProfile": "0xba1132bc08f82fe2" // The account address where the smart contract lives
+})
+
+export default defineNuxtPlugin((nuxtApp) => {
+    nuxtApp.provide("fcl", fcl)
+    nuxtApp.provide("fclArgType", types)
+})
