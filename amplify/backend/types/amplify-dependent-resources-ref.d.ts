00a16973cf47129e7914fc5fb5012e30bf0b130f
田原崇 <taharatakashi@taharatakashinoMacBook-Air-2.local>
2022-12-28 10:01:49 +0900

    Revert "delete Amplify project temporarily"
    This reverts commit 65b6879226a2f912b3f629cd785588bf8e2361e6.

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,29 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "flowgamebuiltonflowe743edd2": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "flowgamebuiltonflow": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "function": {
+        "cronBatchForEvents": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string",
+            "CloudWatchEventRule": "string"
+        }
+    }
+}
